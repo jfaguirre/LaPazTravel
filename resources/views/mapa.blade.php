62c391ac4ebc4de7f5dc9266c0d09cc835ba997a
Monterrosa23 <jose.r.monte2303@gmail.com>
2026-06-22 22:10:26 -0600

    .

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -6,7 +6,7 @@
     <h2>Mapa de La Paz Travel</h2>
     <p>Explora los mejores destinos turísticos, encuentra ofertas exclusivas y planifica tu próxima aventura con nosotros.</p>
     <br>
-    <a class="mapa" href="/"><img src="../resources/images/la-paz-este.png" alt="La paz este"></a>
-    <a class="mapa" href="/"><img src="../resources/images/la-paz-centro.jpg" alt="La paz centro"></a>
-    <a class="mapa" href="/"><img src="../resources/images/la-paz-oeste.png" alt="La paz oeste"></a>
+    <a class="mapa" href="/"><img src="../images/la-paz-este.png" alt="La paz este"></a>
+    <a class="mapa" href="/"><img src="../images/la-paz-centro.jpg" alt="La paz centro"></a>
+    <a class="mapa" href="/"><img src="../images/la-paz-oeste.png" alt="La paz oeste"></a>
 @endsection
