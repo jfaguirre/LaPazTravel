7ef3357640a8fb5dab53a9e0a5c8ef27d3402d6f
Franc Aguirre <66891063+jfaguirre@users.noreply.github.com>
2026-07-10 21:41:29 -0600

    Migraciones

--- database/migrations/2026_07_08_165329_create_publicaciones_table.php
+++ database/migrations/2026_07_08_165329_create_publicaciones_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('publicacions', function (Blueprint $table) {
+        Schema::create('publicaciones', function (Blueprint $table) {
             $table->id();
             $table->string('titulo', 255);
             $table->text('contenido');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('publicacions');
+        Schema::dropIfExists('publicaciones');
     }
 };
